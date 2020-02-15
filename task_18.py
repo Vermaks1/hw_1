ee121b6aeb99ad1f7b9fb2c0e93061c7f4e263fd
Дмитрий Ю. Климов <klimov_dy@edu.kvantorium-pk.ru>
2020-02-15 16:58:48 +1000

    mmmmm

--- task_18.py
+++ task_18.py
@@ -5,20 +5,25 @@ from pyrob.api import *
 
 @task
 def task_8_28():
-    while  wall_is_above() and not wall_is_on_the_right():
-        move_right()
-        if not wall_is_above():
-              while not wall_is_above():
-                  move_up()
-                  while not wall_is_on_the_left():
-                      move_left()
-        elif  while  wall_is_above() and not wall_is_on_the_left():
+    if not wall_is_above():
+        while not wall_is_above():
+            move_up()
+            while not wall_is_on_the_left():
+                move_left()
+    elif wall_is_above():
+        while wall_is_above():
             move_left()
-                while not wall_is_above():
-                    move_up()
-                        while not wall_is_on_the_left():
-                            move_left()
-        
+            while not wall_is_above():
+                move_up()
+                while not wall_is_on_the_left():
+                    move_left()
+    else wall_is_above():
+        while wall_is_above():
+            move_right()
+            while not wall_is_above():
+                move_up()
+                while not wall_is_on_the_left():
+                move_left()
     
       
                 
